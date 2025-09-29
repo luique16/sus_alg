25c9e7cba1f51c36e7822ad04991e2e11e2c8446
Fabio <ganumfilhofabio@gmail.com>
2025-09-29 11:01:06 -0300

    adicionando as funcoes no paciente

--- src/patient.c
+++ src/patient.c
@@ -1,8 +1,19 @@
 #include "../include/history.h"
 #include "../include/patient.h"
 
+#include <string.h>
+
 typedef struct _patient {
     int id;
     char *name;
     HISTORY *history;
 } PATIENT;
+
+PATIENT init_patient(){
+    PATIENT *paciente = (PATIENT*) calloc(1, sizeof(PATIENT));
+    return paciente;
+}
+
+void set_patient_name(PATIENT *patient, char *name){
+    strcpy(patient.name, name);
+}
